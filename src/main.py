3501ee755c1d8ee6f687d1c9686043cb65413c03
lgactna <lgonzalesna@gmail.com>
2025-03-08 21:10:57 -0800

    support for listings, reduced bullet spacing, etc

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from pathlib import Path
 import concurrent.futures
 import re
 import subprocess
+from pathlib import Path
 
 # absolute path to the markdown files to copy here
-SOURCE_MD_DIR = Path("C:/Users/Kisun/Desktop/Obsidian/Academic research/30 - Research topics/39 - By chapter")
+SOURCE_MD_DIR = Path(
+    "C:/Users/Kisun/Desktop/Obsidian/Academic research/30 - Research topics/39 - By chapter"
+)
 # absolute path to the bibliography
 BIBLIOGRAPHY_PATH = Path("./Forensic Image Synthesis.bib")
 
@@ -15,46 +17,67 @@ TARGET_TEX_DIR = Path("./tex")
 
 PANDOC_EXE = Path("C:/Program Files/Pandoc/pandoc.exe")
 
+
 def process_markdown_text(text: str) -> str:
     # Remove all blockquotes
     text = re.sub(r"^>.*", "", text, flags=re.MULTILINE)
-    
+
     # Remove everything inside dataview blocks entirely
-    text = re.sub(r"```dataview.*?```", "\n\n", text, flags=re.DOTALL) 
-    
+    text = re.sub(r"```dataview.*?```", "\n\n", text, flags=re.DOTALL)
+
     # Remove everything inside code blocks, or triple backticks
-    # text = re.sub(r"```.*?```", "\n\nREPLACE CODEBLOCK HERE\n\n", text, flags=re.DOTALL) 
-    
+    # text = re.sub(r"```.*?```", "\n\nREPLACE CODEBLOCK HERE\n\n", text, flags=re.DOTALL)
+
     # Ensure that bulleted lists have two line breaks before
     text = re.sub(r"(.)\n{1,2}((- .*$\n)+)", r"\1\n\n\2", text, flags=re.MULTILINE)
-    
+
     # Ensure that numbered lists have two line breaks before
     text = re.sub(r"(.)\n{1,2}((\d\. .*$\n)+)", r"\1\n\n\2", text, flags=re.MULTILINE)
-    
+
     # Ensure that section headings have two line breaks before and after
-    text = re.sub(r"(.)\n{1,2}(#+ .*$\n)\n{0,}(.)", r"\1\n\n\2\n\n\3", text, flags=re.MULTILINE)
-    
+    text = re.sub(
+        r"(.)\n{1,2}(#+ .*$\n)\n{0,}(.)", r"\1\n\n\2\n\n\3", text, flags=re.MULTILINE
+    )
+
     # Reduce section headings by one level. Require that the heading contains at
     # least two hashes (which avoids Python comments).
     text = re.sub(r"^\#\#", r"#", text, flags=re.MULTILINE)
-    
+
     # Convert wikilinks to bolded text
     text = re.sub(r"\[\[(.*?)\]\]", r"**\1**", text, flags=re.MULTILINE)
-    
+
     # Remove excess line breaks (two or more in a row, flags=re.MULTILINE)
     text = re.sub(r"\n{2,}", r"\n\n", text)
-    
-    # Automatically identify valid URLs (http/https) and convert them to 
+
+    # Automatically identify valid URLs (http/https) and convert them to
     # Markdown hyperlinks, with the URL as the link text.
     text = re.sub(r"(https?://\S+)", r"[\1](\1)", text)
-    
+
     return text
+
 
 def process_latex_text(text: str) -> str:
     # Convert \autocite to \cite
     text = re.sub(r"\\autocite", r"\\cite", text)
-    
+
+    # Special syntax: if an lstlisting's first line contains a line of the form
+    # !lst:caption|label, convert it to a lstlisting with a caption and label
+    text = re.sub(
+        r"\\begin\{lstlisting\}\[(.*?)\]\n!(.*?)\|(.*?)\n",
+        r"\\begin{lstlisting}[\1, label={\2}, caption={\3}]\n",
+        text,
+    )
+
+    # Search for the special syntax \textbf{!lst:(.*?)} and replace it with
+    # \autoref{lst:\1}
+    text = re.sub(r"\\textbf\{!lst:(.*?)\}", r"\\autoref{lst:\1}", text)
+
+    # Citations in code blocks sometimes just don't work, so manually extract
+    # any [@citekey] and replace them with \cite{citkey}
+    text = re.sub(r"\[@(.*?)\]", r"\\cite{\1}", text)
+
     return text
+
 
 def convert_with_pandoc(md_file, tex_file):
     subprocess.run(
@@ -84,15 +107,16 @@ def convert_with_pandoc(md_file, tex_file):
     text = process_latex_text(text)
     tex_file.write_text(text)
 
+
 def substitute_labels(tex_dir: Path) -> dict:
     """
     Find all label tags in the tex files in the given directory,
     then substitute any tags currently inside `\textbf{}` with matching
     label text and convert it to its corresponding `\ref{}` tag, using
     the label.
-    
+
     The result is a dictionary of the text being labeled to the label
-    itself (for example, "\section{1.2.3 - Title}\label{title}" becomes 
+    itself (for example, "\section{1.2.3 - Title}\label{title}" becomes
     {"1.2.3 - Title": "title}).
     """
 
@@ -108,19 +132,24 @@ def substitute_labels(tex_dir: Path) -> dict:
         "39.8 - Future work": "chapter-eight",
         "39.9 - Conclusion": "chapter-nine",
     }
-    
-        
+
     for tex_file in tex_dir.glob("*.tex"):
         text = tex_file.read_text()
         # Forbid the right brace from being part of anything - this is how we'll know
         # if we're not actually next to a label, if there's another brace
         # in between us and the next label tag
-        labels = re.findall(r"^\\\w+?\{([^\{\}]*?)\}\\label\{(.*?)\}$", text, flags=re.DOTALL | re.MULTILINE)
-        
-        label_table.update({k.replace("\n", " "): v.replace("\n", " ") for k, v in labels})
-    
+        labels = re.findall(
+            r"^\\\w+?\{([^\{\}]*?)\}\\label\{(.*?)\}$",
+            text,
+            flags=re.DOTALL | re.MULTILINE,
+        )
+
+        label_table.update(
+            {k.replace("\n", " "): v.replace("\n", " ") for k, v in labels}
+        )
+
     print(label_table)
-    
+
     # With all labels collected, go through the tex files again.
     # Search for any `\textbf{label}`, and check if any of the keys in
     # label_table contain the label. If they do, replace the `\textbf{label}`
@@ -133,24 +162,37 @@ def substitute_labels(tex_dir: Path) -> dict:
         textbfs = [textbf.split("#")[-1] for textbf in textbfs]
         textbfs = [textbf.replace("\n", " ") for textbf in textbfs]
         print(tex_file, textbfs)
-        
+
         # for each textbf, check if it's in the label table
         for textbf in textbfs:
             if textbf in label_table:
                 textbf2 = textbf.replace(" ", "[ \\n]")
-                print(rf"Replacing \\textbf\{{[^\{{\}}]*?{textbf2}\}} with \\autoref{{{label_table[textbf]}}}")
-                
+                print(
+                    rf"Replacing \\textbf\{{[^\{{\}}]*?{textbf2}\}} with \\autoref{{{label_table[textbf]}}}"
+                )
+
                 # if it is, replace it with a ref. note that every space in the textbf
                 # might actually be a newline here, so we have to convert every space
                 # into a regex charset that could be either a space or a newline
-                
-                text = re.sub(rf"\\textbf\{{[^\{{\}}]*?{textbf2}\}}", rf"\\autoref{{{label_table[textbf]}}}", text , flags=re.DOTALL | re.MULTILINE)
-        
+
+                text = re.sub(
+                    rf"\\textbf\{{[^\{{\}}]*?{textbf2}\}}",
+                    rf"\\autoref{{{label_table[textbf]}}}",
+                    text,
+                    flags=re.DOTALL | re.MULTILINE,
+                )
+
         # Final pass - remove anything that looks like "1.2.3 -" inside any
         # tags, because it's already handled by the engine.
-        text = re.sub(r"section\{(?:\d\.)+\d - ", r"section{", text, flags=re.DOTALL | re.MULTILINE)
-        
+        text = re.sub(
+            r"section\{(?:\d\.)+\d - ",
+            r"section{",
+            text,
+            flags=re.DOTALL | re.MULTILINE,
+        )
+
         tex_file.write_text(text)
+
 
 def join_tex_files(tex_dir: Path, output_file: Path, base_file: Path) -> None:
     """
@@ -163,20 +205,22 @@ def join_tex_files(tex_dir: Path, output_file: Path, base_file: Path) -> None:
         "39.4 - Action automation": ("Action automation", "chapter-four"),
         "39.5 - Output and validation": ("Output and validation", "chapter-five"),
         "39.6 - Building scenarios": ("Building scenarios", "chapter-six"),
-        "39.7 - Evaluation and observations": ("Evaluation and observations", "chapter-seven"),
+        "39.7 - Evaluation and observations": (
+            "Evaluation and observations",
+            "chapter-seven",
+        ),
         "39.8 - Future work": ("Future work", "chapter-eight"),
         "39.9 - Conclusion": ("Conclusion", "chapter-nine"),
     }
-    
+
     appendix_labels = {
         "39.A - Architectural diagrams": ("Architectural diagrams", "appendix-a"),
         "39.B - Code samples": ("Code samples", "appendix-b"),
     }
-    
-    
+
     output = "% == Begin thesis content\n"
     appendix = ""
-    
+
     for tex_file in tex_dir.glob("*.tex"):
         if tex_file.stem in chapter_labels:
             chapter_name, label = chapter_labels[tex_file.stem]
@@ -186,16 +230,17 @@ def join_tex_files(tex_dir: Path, output_file: Path, base_file: Path) -> None:
             appendix_name, label = appendix_labels[tex_file.stem]
             appendix += f"\\chapter{{{appendix_name}}}\\label{{{label}}}\n\n"
             appendix += tex_file.read_text() + "\n"
-    
+
     output += "% == End thesis content\n"
-    
+
     text = base_file.read_text()
     text = text.replace("{{thesis_sub_here}}", output)
-    
+
     text = text.replace("{{thesis_appendix_here}}", appendix)
-    
+
     with output_file.open("w") as f:
         f.write(text)
+
 
 if __name__ == "__main__":
     # Start by copying everything in SOURCE_MD_DIR to TARGET_MD_DIR
@@ -207,14 +252,14 @@ if __name__ == "__main__":
         # Process the markdown file
         text = md_file.read_text()
         text = process_markdown_text(text)
-        
+
         # Write the processed markdown file to the target directory
         target_md_file = TARGET_MD_DIR / md_file.name
         target_md_file.write_text(text)
-        
+
     # Invoke pandoc on each markdown file to generate a tex file
     TARGET_TEX_DIR.mkdir(parents=True, exist_ok=True)
-    
+
     # this mangles stdout pretty bad, just set the worker count to 1
     # if needed
     with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -225,9 +270,8 @@ if __name__ == "__main__":
 
         for future in futures:
             future.result()
-    
+
     # now substitute labels
     substitute_labels(TARGET_TEX_DIR)
-    
+
     join_tex_files(TARGET_TEX_DIR, Path("./thesis.tex"), Path("./base.tex"))
-        
